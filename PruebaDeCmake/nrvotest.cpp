bc0fd410a3db2f262e6907b151626eb9781c9d43
Francisco Javier Luque Sugranes <francisco.luque@globant.com>
2022-09-14 09:46:00 -0300

    do tests for nrvo

---
+++ PruebaDeCmake/nrvotest.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+class nrvo_tester {
+private:
+	
+public:
+	int first;
+	int second;
+	int sum;
+	nrvo_tester(int a, int b)
+	{
+		first = a;
+		second = b;
+		sum = a + b;
+
+		cout << "Created" << endl;
+	}
+
+	nrvo_tester(const nrvo_tester &original)
+	{
+		first = original.first;
+		second = original.second;
+		sum = original.sum;
+
+		cout << "Copied" << endl;
+	}
+
+	void print()
+	{
+		cout << "A: " << first << endl;
+		cout << "B: " << second << endl;
+		cout << "A+B: " << sum << endl;
+
+	}
+
+	~nrvo_tester()
+	{
+		cout << "Destroyed" << endl;
+	}
+
+	
+
+};
+
+
+nrvo_tester nrvotest(int a, int b)
+{
+	nrvo_tester resp(a, b);
+
+	return resp;
+}
+
+nrvo_tester copy(nrvo_tester const& a)
+{
+	nrvo_tester resp = a;
+	return resp;
+}
+
+int main()
+{
+	nrvo_tester res = nrvotest(1, 2);
+	nrvo_tester res2 = copy(res);
+	return 0;
+
+}
